e05d754cb0a368286bb0bb621644514e3e8187cc
danielrob1 <168429589+danielrob1@users.noreply.github.com>
2024-10-25 10:56:48 +0200

    Empezado jaxb, hay que acabarlo

---
+++ Ejercicio08.es/src/main/java/jaxb/Ejercicio16.java
@@ -0,0 +1,29 @@
+package jaxb;
+
+import java.io.File;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import jaxb.clasesEjercicio16.Examen;
+
+public class Ejercicio16{
+	private static JAXBContext jC;
+	private static Examen examen;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void unmarshalling() {
+		try {
+			jC = JAXBContext.newInstance(Examen.class);
+			Unmarshaller uM= jC.createUnmarshaller();
+			examen = (Examen) uM.unmarshal(new File("Ejercicio16.xml"));
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
